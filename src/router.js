b2d050ff6b9d5d5315fd3c4bef276c54716d4dc8
git <git@gmail.com>
2025-12-28 16:16:23 +0700

    edit route.js

--- src/router.js
+++ src/router.js
@@ -3,9 +3,9 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 // --- BAGIAN INI SAYA KEMBALIKAN SEPERTI SEMULA ---
 // Pastikan titiknya satu (.) kalau file router sejajar dengan folder components
 // Atau titiknya dua (..) kalau file router ada di dalam folder 'router'
-import Invitation from './components/Invitation.vue'; 
-import Dashboard from './components/Dashboard.vue';
-import QrScanner from './components/QrScanner.vue';
+import Invitation from '@/components/Invitation.vue'; 
+import Dashboard from '@/components/Dashboard.vue';
+import QrScanner from '@/components/QrScanner.vue';
 
 const routes = [
   { 
